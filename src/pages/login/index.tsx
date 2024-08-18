aedad0ac2550caf9d98e2000e9dbf07530f2e018
Kuma1206 <tadakuma369@gmail.com>
2024-08-18 21:39:12 +0900

    ファイル一式

---
+++ src/pages/login/index.tsx
@@ -0,0 +1,14 @@
+import Login from "@/compornents/Login";
+import React from "react";
+
+const login = () => {
+  return (
+    <div>
+      {/*  */}
+      <Login />
+      {/*  */}
+    </div>
+  );
+};
+
+export default login;
---
+++ src/pages/signup/index.tsx
@@ -0,0 +1,16 @@
+import SignUp from "@/compornents/SignUp";
+import Header from "@/compornents/Header";
+import React from "react";
+import Footer from "@/compornents/Footer";
+
+const signup = () => {
+  return (
+    <div>
+      <Header/>
+      <SignUp />
+      <Footer />
+    </div>
+  );
+};
+
+export default signup;
